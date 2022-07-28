664cb0b028ba9e3e897de456b1a512d86c54f59a
artur-rod <tui-dtj@hotmail.com>
2022-07-28 11:54:03 -0300

    ✅ Update Users Service tests

--- src/shared/test/testUtil.ts
+++ src/shared/test/testUtil.ts
@@ -1,11 +1,23 @@
+import { AuthType } from '../../auth/dto/auth.type';
+import { AuthImput } from '../../auth/dto/auth.input';
 import { User } from '../../users/user.entity';
 
 export default class TestUtil {
-  static GiveAValidUser(): User {
+  static ValidUser(): User {
     const user = new User();
     user.email = 'valid@email.com';
     user.name = 'Valid Name LastName';
     user.id = '1';
+
+    return user;
+  }
+
+  static ValidUserToCreate(): User {
+    const user = new User();
+    user.email = 'valid@email.com';
+    user.name = 'Valid Name LastName';
+    user.id = '1';
+    user.password = 'validPassword';
 
     return user;
   }
